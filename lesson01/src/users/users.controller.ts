a9a50d6d33d9b6f37b3a8ed983c687181b4be5fa
Nasir <nasirhussaintormik@gmail.com>
2025-04-04 12:50:57 +0500

    add nodemon dependency and implement basic user CRUD operations in UsersController

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Patch, Delete } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get()
+    findAll() {
+        return 'This action returns a list of all users';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `This action returns a #${id} user`;
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() user: {}) {
+        return { id, ...user }
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return { id }
+    }
+
+}
